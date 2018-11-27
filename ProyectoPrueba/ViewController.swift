5198c06d0abb2286842c28359e7c2fe434f38201
El tiguere <julihoyosm@gmail.com>
2018-11-27 13:28:28 +0100

    Initial Commit

---
+++ ProyectoPrueba/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProyectoPrueba
+//
+//  Created by Julian Hoyos Martinez on 27/11/2018.
+//  Copyright © 2018 Julian Hoyos Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
